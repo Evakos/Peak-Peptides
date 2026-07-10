74e63fdba19c0e412789ef67c991ff555e07c8a3
Christopher Barnard <info@evakos.io>
2026-07-10 10:25:02 +0300

    Remove promo banner for now

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Preloader from '../components/Preloader';
-import PromoBanner from '../components/PromoBanner';
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 import Home from '../pages/Home';
@@ -10,7 +9,6 @@ export default function App() {
   return (
     <div className="min-h-screen bg-black text-white">
       <Preloader />
-      <PromoBanner />
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
